cd22626d6bf3ec112269c9487fccf7017d111784
qdlaoxu <shilong.xu@xitech.com.cn>
2019-04-04 17:49:51 +0800

    update

--- 图片涂鸦/CanvasDoodle.vue
+++ 图片涂鸦/CanvasDoodle.vue
@@ -11,7 +11,15 @@
       <span class="el-icon-rank" title="移动" @click="toolMove" :class="{active:move}"></span>
       <span class="el-icon-edit" title="编辑" @click="toolEdit" :class="{active:edit}"></span>
       <span class="el-icon-back" title="回退" @click="revoke"></span>
-      <span class="el-icon-check" title="确定"></span>
+      <span class="el-icon-check" title="确定" @click="ok"></span>
+    </div>
+    <div class="edit-setting" v-if="edit">
+      <ul class="sizes">
+        <li :class="{active:sizes.current == item}" v-for="(item,index) in sizes.data" :key="index" @click="sizes.current = item"><span :style="{width:item+'px',height:item+'px'}"></span></li>
+      </ul>
+      <ul class="colors">
+        <li :class="{active:colors.current == item}" v-for="(item,index) in colors.data" :key="index" @click="colors.current = item"><span :style="{borderColor:item,background:item}"></span></li>
+      </ul> 
     </div>
   </div>
 </template>
@@ -28,6 +36,14 @@ export default {
       offset: {
         x: 0,
         y: 0
+      },
+      sizes:{
+        current:6,
+        data:[6,10,14]
+      },
+      colors:{
+        current:"#ff0f00",
+        data:["#ff0f00","#ffbe00","#1a9bff","#1aad19","#4d4d4d"]
       },
 
       // canvas
@@ -64,20 +80,23 @@ export default {
         }, 100);
       };
     },
+    // 鼠标在画布上按下
     canvasDown(e) {
-      this.ctx.lineWidth = 10;
-      this.ctx.shadowBlur = 10;
-      this.ctx.shadowColor = "red";
-      this.ctx.strokeStyle = "red";
       this.flag = true;
+      // this.ctx.shadowBlur = 10;
+      // this.ctx.shadowColor = "red";
+      this.ctx.lineWidth = this.sizes.current;
+      this.ctx.strokeStyle = this.colors.current;
       console.log(e.offsetX, e.offsetY);
       this.ctx.beginPath();
       this.ctx.moveTo(e.offsetX, e.offsetY);
     },
+    // 鼠标在画布上松开
     canvasUp() {
       this.flag = false;
       this.imgData.push(this.ctx.getImageData(0, 0, this.width, this.height));
     },
+    // 鼠标在画布上移动
     canvasMove(e) {
       if (this.flag) {
         this.ctx.lineTo(e.offsetX, e.offsetY);
@@ -94,15 +113,17 @@ export default {
         this.imgData.pop();
       }
     },
-
-    toolMove() {
-      this.move = true;
-      this.edit = false;
-    },
+    // 启用编辑模式
     toolEdit() {
       this.edit = true;
       this.move = false;
     },
+    // 启用拖动模式
+    toolMove() {
+      this.move = true;
+      this.edit = false;
+    },
+    // 鼠标在拖拽层按下
     layerDown(e) {
       this.moveFlag = true;
       this.offset = {
@@ -114,6 +135,7 @@ export default {
       console.log("left", canvasBox.scrollLeft);
       console.log("top", canvasBox.scrollTop);
     },
+    // 鼠标在拖拽层拖动
     layerMove(e) {
       if (this.moveFlag) {
         let x = this.offset.x - e.offsetX + this.offset.oldx;
@@ -121,8 +143,13 @@ export default {
         canvasBox.scroll(x, y);
       }
     },
+    // 鼠标在拖拽层松开
     layerUp() {
       this.moveFlag = false;
+    },
+    ok(){
+      var _url = canvas.toDataURL('image/png');
+      console.log(_url);
     }
   }
 };
@@ -166,6 +193,51 @@ export default {
     }
     &.active {
       color: #000;
+    }
+  }
+}
+.edit-setting{
+  display: inline-block;
+  padding: 0 40px;
+  background: #f5f5f5;
+  ul{
+    margin: 0 10px;
+    padding:5px 0 10px;
+  }
+  ul,li{
+    display: inline-block;
+    vertical-align: middle;
+  }
+  li{
+    width: 20px;
+    margin:0 2px;
+    cursor: pointer;
+  }
+  .sizes{
+    span{
+      background: #ccc;
+      display: block;
+      border-radius: 50%;
+    }
+    li.active{
+      span{
+        background: #9083ED !important;
+      }
+    }
+  }
+  .colors {
+    span{
+      display: block;
+      width:16px;
+      height: 16px;
+      border-style: solid;
+      border-width: 4px;
+      box-sizing: border-box;
+    }
+    li.active{
+      span{
+        background: #fff !important;
+      }
     }
   }
 }
